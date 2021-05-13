983c8d2297d50cf521fc8d36883c576003b170fb
lemade <made@honkgong.info>
2021-05-13 17:59:26 +0200

    Add sign up form

--- src/App.js
+++ src/App.js
@@ -1,10 +1,49 @@
-import React from "react";
+import axios from "axios";
+import config from "./config";
+import { React, useState } from "react";
+import { Route, Switch, withRouter } from "react-router-dom";
+import { SignUp } from "./components";
 
-function App() {
+function App(props) {
+  const [user, updateUser] = useState(null);
+  const [error, updateError] = useState(null);
+
+  const handleSignUp = (e) => {
+    e.preventDefault();
+    const { name, password, passwordConfirmation } = e.target;
+    let newUser = {
+      name: name.value,
+      password: password.value,
+      passwordConfirmation: passwordConfirmation.value,
+    };
+
+    axios
+      .post(`${config.API_URL}/api/auth/signup`, newUser, {
+        withCredentials: true,
+      })
+      .then((response) => {
+        updateUser(response.data);
+        updateError(null);
+      })
+      .catch((err) => {
+        updateError(err.response.data);
+      });
+  };
+
   return (
-    <div>
-    </div>
+    <>
+      <Switch>
+        <Route
+          path="/signup"
+          render={(routeProps) => {
+            return (
+              <SignUp error={error} onSubmit={handleSignUp} {...routeProps} />
+            );
+          }}
+        />
+      </Switch>
+    </>
   );
 }
 
-export default App;
+export default withRouter(App);
---
+++ src/components/SignUp.js
@@ -0,0 +1,120 @@
+import React from "react";
+import {
+  makeStyles,
+  Avatar,
+  Button,
+  Container,
+  Grid,
+  Link,
+  TextField,
+  Typography,
+} from "@material-ui/core";
+import Alert from "@material-ui/lab/Alert";
+
+const useStyles = makeStyles((theme) => ({
+  alert: {
+    marginBottom: theme.spacing(3)
+  },
+  paper: {
+    marginTop: theme.spacing(8),
+    display: "flex",
+    flexDirection: "column",
+    alignItems: "center",
+  },
+  avatar: {
+    margin: theme.spacing(1),
+  },
+  description: {
+    margin: theme.spacing(3),
+    textAlign: "center",
+    flexGrow: 1,
+  },
+  form: {
+    width: "100%",
+    marginTop: theme.spacing(3),
+  },
+  submit: {
+    margin: theme.spacing(3, 0, 2),
+  },
+}));
+
+function SignUp(props) {
+  const { onSubmit, error } = props;
+  const classes = useStyles();
+
+  return (
+    <Container maxWidth="xs">
+      <div className={classes.paper}>
+        <Avatar
+          className={classes.avatar}
+          src="/public/logo512.png"
+          alt="moep-avatar"
+        />
+        <Typography component="h1" variant="h3">
+          MOEP
+        </Typography>
+        <Typography component="body1" className={classes.description}>
+          Nullam accumsan lorem in dui. Nulla porta dolor. Etiam imperdiet
+          imperdiet orci.
+        </Typography>
+        <Typography component="h2" variant="h5">
+          Sign Up
+        </Typography>
+      </div>
+      <form onSubmit={onSubmit} className={classes.form} noValidate>
+        {error && <Alert className={classes.alert} severity="error">{error.errorMessage}</Alert>}
+        <Grid container spacing={2}>
+          <Grid item xs={12}>
+            <TextField
+              name="name"
+              variant="outlined"
+              required
+              fullWidth
+              label="Username"
+              autoFocus
+            />
+          </Grid>
+
+          <Grid item xs={12}>
+            <TextField
+              variant="outlined"
+              required
+              fullWidth
+              name="password"
+              label="Password"
+              type="password"
+            />
+          </Grid>
+          <Grid item xs={12}>
+            <TextField
+              variant="outlined"
+              required
+              fullWidth
+              name="passwordConfirmation"
+              label="Password Confirmation"
+              type="password"
+            />
+          </Grid>
+        </Grid>
+        <Button
+          type="submit"
+          fullWidth
+          variant="contained"
+          color="primary"
+          className={classes.submit}
+        >
+          Sign Up
+        </Button>
+        <Grid container justify="flex-end">
+          <Grid item>
+            <Link to="/" variant="body2">
+              Already have an account? Sign in
+            </Link>
+          </Grid>
+        </Grid>
+      </form>
+    </Container>
+  );
+}
+
+export default SignUp;
