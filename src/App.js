0515f9a499ed48f30966577e17e6d3cc335984ce
lemade <made@honkgong.info>
2021-05-13 20:07:45 +0200

    Add sign in

--- src/App.js
+++ src/App.js
@@ -2,7 +2,7 @@ import axios from "axios";
 import config from "./config";
 import { React, useState } from "react";
 import { Route, Switch, withRouter } from "react-router-dom";
-import { SignUp } from "./components";
+import { SignUp, SignIn } from "./components";
 
 function App(props) {
   const [user, updateUser] = useState(null);
@@ -30,9 +30,37 @@ function App(props) {
       });
   };
 
+  const handleSignIn = async (e) => {
+    e.preventDefault();
+    const { name, password } = e.target;
+    let user = {
+      name: name.value,
+      password: password.value,
+    };
+
+    axios
+      .post(`${config.API_URL}/api/auth/signin`, user, { withCredentials: true })
+      .then((response) => {
+        updateUser(response.data);
+        updateError(null);
+      })
+      .catch((err) => {
+        updateError(err.response.data);
+      });
+  };
+
   return (
     <>
       <Switch>
+        <Route
+          exact
+          path="/"
+          render={(routeProps) => {
+            return (
+              <SignIn error={error} onSubmit={handleSignIn} {...routeProps} />
+            );
+          }}
+        />
         <Route
           path="/signup"
           render={(routeProps) => {
