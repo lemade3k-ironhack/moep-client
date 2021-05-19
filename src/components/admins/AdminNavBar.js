149b8fcb0b52a522b70f4835ac88584e90c21394
lemade <made@honkgong.info>
2021-05-19 21:59:06 +0200

    Add styles to admin components

--- src/components/admins/AdminNavBar.js
+++ src/components/admins/AdminNavBar.js
@@ -4,21 +4,19 @@ import { Link } from "react-router-dom";
 import { Navbar, Nav, NavDropdown } from "react-bootstrap";
 
 function AdminNavBar(props) {
-  const { onLogout } = props;
+  const { user, onLogout } = props;
   const festivalName = config.FESTIVAL_NAME;
 
   return (
-    <Navbar bg="light" expand="lg">
+    <Navbar className="navbar" expand="lg">
       <Navbar.Brand>
-        <Link to="/admin">{festivalName}</Link>
+        <Link to="/admin">{festivalName} Planer - Admin CMS</Link>
       </Navbar.Brand>
 
-      <Nav className="mr-auto">
-        <NavDropdown title="Menu" id="basic-nav-dropdown">
-          <NavDropdown.Item variant="outline-success" onClick={onLogout}>
-            Logout
-          </NavDropdown.Item>
-        </NavDropdown>
+      <Nav>
+        <Nav.Link variant="outline-success" onClick={onLogout}>
+          Logout {user.name}
+        </Nav.Link>
       </Nav>
     </Navbar>
   );
