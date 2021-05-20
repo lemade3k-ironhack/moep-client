5b477067a8f526932761ff7d523d24b0302794a9
Anna Tielke <anna_tielke@gmx.de>
2021-05-20 21:18:47 +0200

    notfoundstyle

--- src/components/notFound/NotFound.js
+++ src/components/notFound/NotFound.js
@@ -1,14 +1,20 @@
 import React, { Component } from "react";
 import data from "./animation/animation.json";
 import LottieControl from "./LottieControl";
+import { Link } from "react-router-dom";
 
 class NotFound extends Component {
   render() {
     return (
       <div>
-        <h2>404</h2>
-        <h5>you are at the wrong campsite, the festival is not here</h5>
-        <LottieControl animation={data} />
+        <h1 className="center linkColor">404</h1>
+        <h5 className="center linkColor">
+          you are at the wrong campsite, the festival is not here
+        </h5>
+        <Link to="/welcome" className="center linkColor">
+          GO HOME!
+        </Link>
+        <LottieControl animation={data} style={{ backgroundSize: "cover" }} />
       </div>
     );
   }
